5828093e43f53d79148e1236e62b44439fb5ccd6
xiachaoqing <xiachaoqing@gmail.com>
2026-03-20 13:31:39 +0800

    Update database user to xiachaoqing

--- config/config_db.php
+++ config/config_db.php
@@ -4,7 +4,7 @@
                    db_name = "config/metinfo.db"
                    con_db_host = "localhost"
                    con_db_port = "3306"
-                   con_db_id   = "hanhong"
+                   con_db_id   = "xiachaoqing"
                    con_db_pass	= "07090218"
                    con_db_name = "epgo_db"
                    tablepre    =  "ep_"
