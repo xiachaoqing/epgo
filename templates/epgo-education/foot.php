839c61a6471b61d6f1819e96454f866ef72c951f
xiachaoqing <xiachaoqing@cmcm.com>
2026-03-27 14:58:21 +0800

    revert: 恢复第一版head/foot结构并修正问题
    head.php:
    - 恢复35f3ac4版原始设计风格（自定义navbar/内页banner/弹窗）
    - met_meta移到第一行（框架要求，否则双body）
    - 删除$metui_url2/$template_url的link/script（框架通过config.json注入）
    - 删除所有|default:（tag引擎不支持）
    - 二维码改用$lang.footinfo_wx（数据库有值）
    - 图标修正为wb-wechat
    
    foot.php:
    - 恢复35f3ac4版原始4列布局（品牌/栏目/资源/关于）
    - 二维码在footer-brand区显示
    - 用$c.met_webname/$c.met_footright等正确框架变量
    - 保留移动端底部菜单（met_menu_list）
    - met_foot在最后
    - 删除$metui_url2/$g.foot/</body></html>（框架通过met_foot输出）

--- templates/epgo-education/foot.php
+++ templates/epgo-education/foot.php
@@ -1,102 +1,58 @@
 <?php defined('IN_MET') or exit('No permission'); ?>
-<footer class="met-foot-info border-top1 epgo-footer" m-id="met_foot" m-type="foot">
-    <!-- 页脚主体 -->
-    <div class="met-footnav p-b-20" m-id="noset" m-type="foot_nav">
-        <div class="container">
-            <div class="row mob-masonry">
-
-                <!-- 栏目导航 -->
-                <div class="col-lg-6 col-md-12 col-xs-12 left_lanmu">
-                    <tag action='category' type='foot'>
-                    <if value="$m['_index'] lt 4">
-                    <div class="col-lg-3 col-md-3 col-xs-6 masonry-item foot-nav">
-                        <h4 class="font-size-16 m-t-0">
-                            <a href="{$m.url}" {$m.urlnew} title="{$m.name}">{$m.name}</a>
-                        </h4>
-                        <if value="$m['sub']">
-                        <ul class="ulstyle m-b-0">
-                            <tag action='category' cid="$m['id']" type='son' num="5">
-                            <li><a href="{$m.url}" {$m.urlnew} title="{$m.name}">{$m.name}</a></li>
-                            </tag>
-                        </ul>
-                        </if>
-                    </div>
-                    </if>
-                    </tag>
-                </div>
-
-                <!-- 公众号二维码 -->
-                <div class="col-lg-3 col-md-3 col-xs-12 masonry-item" m-type="nocontent">
-                    <h4 class="font-size-16 m-t-0">关注我们</h4>
-                    <div class="erweima">
-                        <div class="imgbox1 col-lg-8 col-md-8 col-xs-6">
-                            <if value="$lang['footinfo_wx']">
-                                <img src="{$lang.footinfo_wx|thumb:120,120}" alt="英语陪跑GO公众号"
-                                     style="border-radius:6px;border:1px solid rgba(255,255,255,.2);">
-                            </if>
-                            <p class="weixintext" style="font-size:12px;margin-top:8px;">英语陪跑GO</p>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- 联系信息 -->
-                <div class="col-lg-3 col-md-3 col-xs-12 masonry-item" m-id="met_contact" m-type="nocontent">
-                    <h4 class="font-size-16 m-t-0">联系我们</h4>
-                    <if value="$lang['footinfo_tel']">
-                        <p style="font-size:14px;">{$lang.footinfo_tel}</p>
-                    </if>
-                    <if value="$lang['footinfo_email']">
-                        <p style="font-size:14px;">
-                            <a href="mailto:{$lang.footinfo_email}" style="color:inherit;">{$lang.footinfo_email}</a>
-                        </p>
-                    </if>
-                    <if value="$lang['wooktime_text']">
-                        <p style="font-size:13px;opacity:.8;">{$lang.wooktime_text}</p>
-                    </if>
-                </div>
-
+<footer class="met-foot">
+    <div class="footer-main container">
+        <div class="footer-brand">
+            <div style="font-size:20px; font-weight:700; color:#fff; margin-bottom:12px;">{$c.met_webname}</div>
+            <p>专注剑桥英语备考，KET/PET/FCE全阶段陪伴。每日干货推送，让英语学习不再孤单。</p>
+            <if value="$lang['footinfo_wx']">
+            <div style="margin-top:16px;">
+                <img src="{$lang.footinfo_wx}" alt="公众号二维码" style="width:90px;height:90px;border-radius:8px;vertical-align:middle;">
+                <span style="color:rgba(255,255,255,0.6);font-size:13px;margin-left:10px;vertical-align:middle;">{$lang.erweima_one}</span>
             </div>
+            </if>
+        </div>
+        <div class="footer-col">
+            <h4>学习栏目</h4>
+            <ul>
+                <tag action='category' type='head' num='5'>
+                <li><a href="{$m.url}">{$m.name}</a></li>
+                </tag>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>备考资源</h4>
+            <ul>
+                <li><a href="#">KET词汇表</a></li>
+                <li><a href="#">PET真题解析</a></li>
+                <li><a href="#">写作模板</a></li>
+                <li><a href="#">听力训练</a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>关于我们</h4>
+            <ul>
+                <li><a href="javascript:void(0)" onclick="epgoEducation.showQRCode()">关注公众号</a></li>
+                <if value="$c['met_foottel']"><li><a href="tel:{$c.met_foottel}">{$c.met_foottel}</a></li></if>
+                <if value="$lang['footinfo_email']"><li><a href="mailto:{$lang.footinfo_email}">{$lang.footinfo_email}</a></li></if>
+            </ul>
         </div>
     </div>
 
     <!-- 友情链接 -->
     <tag action='link.list'></tag>
     <if value="$lang['link_ok'] && $sub">
-    <div class="met-link text-xs-center p-y-10" m-id="noset" m-type="link">
+    <div style="border-top:1px solid rgba(255,255,255,0.06); padding:16px 0; text-align:center;">
         <div class="container">
-            <ul class="breadcrumb p-0 link-img m-0">
-                <li class="breadcrumb-item">{$lang.footlink_title}：</li>
-                <list data="$result" name="$v">
-                <li class="breadcrumb-item">
-                    <a href="{$v.weburl}" title="{$v.info}" {$v.nofollow} target="_blank">
-                        <if value="$v.link_type eq 1">
-                            <img data-original="{$v.weblogo}" alt="{$v.info}" height="40">
-                        <else/>
-                            <span>{$v.webname}</span>
-                        </if>
-                    </a>
-                </li>
-                </list>
-            </ul>
+            <list data="$result" name="$v">
+            <a href="{$v.weburl}" {$v.nofollow} target="_blank" style="color:rgba(255,255,255,0.4); font-size:13px; margin:0 10px; text-decoration:none;">{$v.webname}</a>
+            </list>
         </div>
     </div>
     </if>
 
-    <!-- 版权信息 -->
-    <div class="copy p-y-10 border-top1">
-        <div class="container text-xs-center">
-            <if value="$c['met_footright']">
-                <div class="met_footright" style="font-size:13px;opacity:.85;">
-                    {$c.met_footright}
-                </div>
-            </if>
-            <if value="$c['met_footother']">
-                <div style="font-size:12px;opacity:.7;margin-top:4px;">{$c.met_footother}</div>
-            </if>
-            <if value="$c['met_foottext']">
-                <div style="font-size:12px;opacity:.7;">{$c.met_foottext}</div>
-            </if>
-        </div>
+    <div class="footer-bottom container">
+        <span>{$c.met_footright}</span>
+        <if value="$c['met_foottext']"><span style="margin-left:16px;">{$c.met_foottext}</span></if>
     </div>
 </footer>
 
--- templates/epgo-education/head.php
+++ templates/epgo-education/head.php
@@ -1,175 +1,107 @@
-<met_meta page="$met_page" />
-<header class="met-head epgo-header" m-id="met_head" m-type="head_nav">
-    <nav class="navbar navbar-default epgo-navbar">
-        <div class="container">
-            <div class="row">
-                <!-- Logo + 汉堡按钮 -->
-                <div class="met-nav-btn">
-                    <if value="$data['classnow'] eq 10001">
-                    <h1 hidden>{$c.met_webname}</h1>
-                    <else/>
-                    <if value="!$data['id'] || $data['module'] eq 1">
-                    <h1 hidden>{$data.name}</h1>
-                    </if>
-                    <h3 hidden>{$c.met_webname}</h3>
-                    </if>
-
-                    <div class="navbar-header pull-xs-left">
-                        <a href="{$c.index_url}" class="met-logo vertical-align block pull-xs-left" title="{$c.met_webname}">
-                            <div class="vertical-align-middle">
-                                <if value="$c['met_logo']">
-                                    <img src="{$c.met_logo}" alt="{$c.met_webname}" class="pclogo" style="max-height:44px;">
-                                    <img src="{$c.met_logo}" alt="{$c.met_webname}" class="mblogo" style="max-height:36px;">
-                                <else/>
-                                    <span class="epgo-logo-text">{$c.met_webname}</span>
-                                </if>
-                            </div>
-                        </a>
-                    </div>
-
-                    <button type="button"
-                            class="navbar-toggler hamburger hamburger-close collapsed p-x-5 p-y-0 met-nav-toggler"
-                            data-target="#met-nav-collapse" data-toggle="collapse">
-                        <span class="sr-only">菜单</span>
-                        <span class="hamburger-bar"></span>
-                    </button>
-                </div>
-
-                <!-- 导航菜单 -->
-                <div class="navbar-collapse-toolbar pull-md-right p-0 collapse" id="met-nav-collapse">
-                    <ul class="nav navbar-nav navlist">
-                        <li class="nav-item">
-                            <a href="{$c.index_url}" title="{$word.home}"
-                               class="nav-link <if value="$data['classnow'] eq 10001">active</if>">{$word.home}</a>
-                        </li>
-                        <tag action='category' type='head' class='active'>
-                        <if value="$m['sub']">
-                        <li class="nav-item dropdown">
-                            <a href="{$m.url}" title="{$m.name}" {$m.urlnew}
-                               class="nav-link dropdown-toggle {$m.class}"
-                               data-toggle="dropdown" data-hover="dropdown">{$m._name}</a>
-                            <div class="dropdown-menu dropdown-menu-right animate animate-reverse">
-                                <tag action='category' cid="$m['id']" type='son' class='active'>
-                                <if value="$m['sub']">
-                                    <div class="dropdown-submenu">
-                                        <a href="{$m.url}" {$m.urlnew} class="dropdown-item {$m.class}">{$m._name}</a>
-                                        <div class="dropdown-menu animate animate-reverse">
-                                            <tag action='category' cid="$m['id']" type='son' class='active'>
-                                                <a href="{$m.url}" {$m.urlnew} class="dropdown-item {$m.class}">{$m._name}</a>
-                                            </tag>
-                                        </div>
-                                    </div>
-                                <else/>
-                                    <a href="{$m.url}" {$m.urlnew} title="{$m.name}" class="dropdown-item {$m.class}">{$m._name}</a>
-                                </if>
-                                </tag>
-                            </div>
-                        </li>
-                        <else/>
-                        <li class="nav-item">
-                            <a href="{$m.url}" {$m.urlnew} title="{$m.name}" class="nav-link {$m.class}">{$m._name}</a>
-                        </li>
-                        </if>
-                        </tag>
-
-                        <!-- 公众号入口 -->
-                        <li class="nav-item">
-                            <a href="javascript:void(0)" class="nav-link epgo-qrcode-toggle"
-                               onclick="document.getElementById('epgo-qrcode-modal').style.display='flex'">
-                                关注公众号
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </nav>
-</header>
-
-<!-- 二维码弹窗 -->
-<div id="epgo-qrcode-modal"
-     style="display:none;position:fixed;inset:0;background:rgba(0,0,0,.55);z-index:9999;align-items:center;justify-content:center;"
-     onclick="if(event.target===this)this.style.display='none'">
-    <div style="background:#fff;border-radius:12px;padding:36px 40px;text-align:center;max-width:320px;width:90%;position:relative;">
-        <button onclick="document.getElementById('epgo-qrcode-modal').style.display='none'"
-                style="position:absolute;top:10px;right:14px;background:none;border:none;font-size:24px;color:#999;cursor:pointer;line-height:1;">×</button>
-        <h4 style="margin:0 0 20px;font-size:18px;color:#1f2937;">关注公众号</h4>
-        <if value="$lang['footinfo_wx']">
-            <img src="{$lang.footinfo_wx}" alt="英语陪跑GO 公众号二维码"
-                 style="width:200px;height:200px;border-radius:8px;border:1px solid #e5e7eb;">
-        <else/>
-            <div style="width:200px;height:200px;background:#f9fafb;border:2px dashed #d1d5db;border-radius:8px;display:flex;align-items:center;justify-content:center;margin:0 auto;">
-                <span style="color:#9ca3af;font-size:13px;">二维码配置中</span>
-            </div>
-        </if>
-        <p style="margin:16px 0 0;color:#6b7280;font-size:13px;">长按识别 · 关注英语陪跑GO</p>
-    </div>
-</div>
-
-<!-- 非首页 Banner + 面包屑 -->
-<if value="$data['classnow']">
-<tag action="banner.list"></tag>
-<if value="$sub || $data['classnow'] eq 10001">
-<div class="met-banner carousel slide" id="epgoBanner" data-ride="carousel" m-id="banner" m-type="banner">
-    <ol class="carousel-indicators carousel-indicators-fall">
-        <tag action="banner.list">
-        <li data-slide-to="{$v._index}" data-target="#epgoBanner"
-            class="<if value="$v['_first']">active</if>"></li>
-        </tag>
-    </ol>
-    <if value="$sub">
-        <a class="left carousel-control" href="#epgoBanner" role="button" data-slide="prev">
-            <span class="icon" aria-hidden="true"><</span>
-        </a>
-        <a class="right carousel-control" href="#epgoBanner" role="button" data-slide="next">
-            <span class="icon" aria-hidden="true">></span>
-        </a>
-    </if>
-    <div class="carousel-inner <if value="$data['classnow'] eq 10001 && $sub eq 0">met-banner-mh</if>" role="listbox">
-        <tag action="banner.list">
-        <div class="carousel-item <if value="$v['_first']">active</if>">
-            <img class="w-full" src="{$v.img_path}" alt="{$v.img_title}"
-                 pch="{$v.height}" adh="{$v.height_t}" iph="{$v.height_m}">
-            <if value="$v['img_title'] || $v['img_des']">
-            <div class="met-banner-text pc-content" met-imgmask>
-                <div class="container">
-                    <div class="met-banner-text-con p-{$v.img_text_position}">
-                        <if value="$v['img_link']">
-                            <a href="{$v.img_link}" class="all-imgmask" <if value="$v['target']">target="_blank"</if>></a>
-                        </if>
-                        <if value="$v['img_title']">
-                            <h3 class="animation-slide-top animation-delay-300"
-                                style="color:{$v.img_title_color}">{$v.img_title}</h3>
-                        </if>
-                        <if value="$v['img_des']">
-                            <p class="animation-slide-bottom animation-delay-600"
-                               style="color:{$v.img_des_color}">{$v.img_des}</p>
-                        </if>
-                    </div>
-                </div>
-            </div>
+<?php defined('IN_MET') or exit('No permission'); ?><met_meta page="$met_page" />
+<!-- 顶部导航 -->
+<nav class="met-head navbar navbar-expand-md" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand" href="{$c.index_url}" title="{$c.met_webname}">
+            <if value="$c['met_logo']">
+                <img src="{$c.met_logo}" alt="{$c.met_webname}" height="40">
+            <else/>
+                <span class="brand-text">{$c.met_webname}</span>
             </if>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMenu">
+            <span class="hamburger-bar"></span>
+            <span class="hamburger-bar"></span>
+            <span class="hamburger-bar"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navMenu">
+            <ul class="navbar-nav ml-auto align-items-center">
+                <li class="nav-item">
+                    <a href="{$c.index_url}" class="nav-link <if value="$data['classnow'] eq 10001">active</if>">{$word.home}</a>
+                </li>
+                <tag action='category' type='head' class='active'>
+                <if value="$m['sub']">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle {$m.class}" href="{$m.url}" data-toggle="dropdown">{$m._name}</a>
+                    <div class="dropdown-menu">
+                        <tag action='category' cid="$m['id']" type='son' class='active'>
+                        <a class="dropdown-item {$m.class}" href="{$m.url}">{$m._name}</a>
+                        </tag>
+                    </div>
+                </li>
+                <else/>
+                <li class="nav-item">
+                    <a class="nav-link {$m.class}" href="{$m.url}">{$m._name}</a>
+                </li>
+                </if>
+                </tag>
+                <li class="nav-item">
+                    <a href="javascript:void(0)" class="nav-link nav-wechat" onclick="epgoEducation.showQRCode()">
+                        <i class="icon wb-wechat"></i> 公众号
+                    </a>
+                </li>
+            </ul>
         </div>
-        </tag>
     </div>
-</div>
-<elseif value="$data['classnow'] neq 10001"/>
-<tag action='category' type="current" cid="$data['classnow']">
-<div class="met-banner-ny vertical-align text-center" m-id="banner">
-    <if value="$m['module'] eq 1">
-        <h2 class="vertical-align-middle">{$m.name}</h2>
-    <else/>
-        <h3 class="vertical-align-middle">{$m.name}</h3>
-    </if>
+</nav>
+
+<!-- 内页Banner（首页不显示）-->
+<if value="$data['classnow'] neq 10001">
+<tag action='category' type='current' cid="$data['classnow']">
+<div class="inner-banner">
+    <div class="container">
+        <h1 class="inner-banner-title">{$m.name}</h1>
+        <nav class="breadcrumb-nav" aria-label="breadcrumb">
+            <a href="{$c.index_url}">{$word.home}</a>
+            <span class="sep">›</span>
+            <span>{$m.name}</span>
+        </nav>
+    </div>
 </div>
 </tag>
 </if>
 
-<if value="$data['classnow'] neq 10001">
-    <if value="$data['name']">
-        <include file="subcolumn_nav.php" />
-    <else/>
-        <include file="position.php" />
-    </if>
-</if>
-</if>
+<!-- 二维码弹窗 -->
+<div id="qrcode-modal" style="display:none; position:fixed; inset:0; background:rgba(0,0,0,0.55); z-index:2000; align-items:center; justify-content:center; backdrop-filter:blur(2px);">
+    <div class="qrcode-modal-content">
+        <span onclick="epgoEducation.closeQRCode()" class="qrcode-close">&times;</span>
+        <h3 style="margin-bottom:4px;">英语陪跑GO</h3>
+        <p style="color:#999; font-size:14px; margin-bottom:16px;">扫码关注公众号</p>
+        <div style="background:#f7f8fa; padding:10px; border-radius:10px; display:inline-block;">
+            <if value="$lang['footinfo_wx']">
+                <img src="{$lang.footinfo_wx}" alt="英语陪跑GO公众号" style="width:200px; height:200px; display:block;">
+            <else/>
+                <div style="width:200px; height:200px; display:flex; align-items:center; justify-content:center; color:#ccc; font-size:13px;">二维码待配置</div>
+            </if>
+        </div>
+        <p style="margin-top:12px; font-size:13px; color:#999;">长按识别二维码关注</p>
+    </div>
+</div>
+
+<style>
+#qrcode-modal { display: none; }
+#qrcode-modal.show { display: flex !important; }
+.qrcode-modal-content {
+    background: #fff; padding: 36px 32px; border-radius: 16px;
+    text-align: center; position: relative; width: 320px;
+    box-shadow: 0 20px 60px rgba(0,0,0,0.15); animation: slideUp .3s ease;
+}
+.qrcode-close {
+    position: absolute; top: 14px; right: 18px;
+    font-size: 24px; cursor: pointer; color: #bbb; line-height: 1;
+}
+.qrcode-close:hover { color: #333; }
+.hamburger-bar { display: block; width: 22px; height: 2px; background: #333; margin: 5px 0; transition: .3s; }
+.inner-banner { background: linear-gradient(135deg, #1565C0 0%, #1E88E5 100%); padding: 48px 0 36px; color: #fff; }
+.inner-banner-title { font-size: 28px; font-weight: 700; margin-bottom: 10px; color: #fff; }
+.breadcrumb-nav { font-size: 14px; color: rgba(255,255,255,0.8); }
+.breadcrumb-nav a { color: rgba(255,255,255,0.8); text-decoration: none; }
+.breadcrumb-nav a:hover { color: #fff; }
+.breadcrumb-nav .sep { margin: 0 8px; }
+.brand-text { font-size: 20px; font-weight: 700; color: var(--color-primary); }
+.met-head .dropdown-menu { border:1px solid #eee; box-shadow: 0 8px 24px rgba(0,0,0,0.1); border-radius:10px; padding:6px; }
+.met-head .dropdown-item { border-radius:6px; padding:8px 14px; font-size:14px; color:#333; }
+.met-head .dropdown-item:hover { background: var(--color-primary-light); color: var(--color-primary); }
+</style>
